cdf9e4c7489c4ccf71283e83bdcac612fcceebac
Ilya Protasov <ilya.protasov@omega-r.com>
2019-02-28 20:55:45 +0300

    added working lab2

---
+++ lab2/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(string data)
+        {
+            string id = null;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("http://localhost:5000/");
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/values", data);
+            id = await response.Content.ReadAsStringAsync();
+
+            return Ok(id);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
